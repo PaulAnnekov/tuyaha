2908ce2b00d6e8b787f6285f2adab47251e24ca5
Paul Annekov <paul.annekov@gmail.com>
2019-06-28 15:41:16 +0300

    Added license meta

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     url="https://github.com/PaulAnnekov/tuyaha",
+    license='MIT',
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
