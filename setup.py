3db7cf23747ab77f6baf55ea1fbd152280422082
Paul Annekov <paul.annekov@gmail.com>
2019-06-28 14:17:53 +0300

    Added link to github in setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    url="https://github.com/PaulAnnekov/tuyaha",
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
