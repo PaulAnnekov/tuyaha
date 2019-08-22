48fbaea90569d997a2384e81cbc46c4b3b9b3e37
Madalin Tache <madalin@madalin.eu>
2019-08-22 11:09:31 +0300

    Minor change in order to fix the switch status

--- tuyaha/devices/switch.py
+++ tuyaha/devices/switch.py
@@ -1,10 +1,14 @@
 from tuyaha.devices.base import TuyaDevice
 
-
 class TuyaSwitch(TuyaDevice):
 
     def state(self):
-        state = self.data.get('state')
+        devices = self.api.discover_devices()
+        state = None
+        for device in devices:
+            if device['id'] == self.obj_id:
+                state = device['data']['state']
+                return state
         if state is None:
             return None
         return state
