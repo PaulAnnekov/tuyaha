7c57b9b3eeeb60ae3b16461394273818810e3244
Helldog136 <130792@student.umons.ac.be>
2020-06-01 17:58:24 +0200

    added state cache to light objects because tuya api doesn't seem to return the information anymore. This shouldn't cause problems as long as the user doesn't use another app to control his lights (this is unlikely with home assistant and is better than an exception anyway)

--- tuyaha/devices/light.py
+++ tuyaha/devices/light.py
@@ -2,12 +2,18 @@ from tuyaha.devices.base import TuyaDevice
 
 
 class TuyaLight(TuyaDevice):
+    def __init__(self):
+        self.last_hs_color = None
+        self.last_brightness = None
+        self.last_color_temp = None
+        self.last_state = None
+
     def state(self):
         state = self.data.get("state")
-        if state == "true":
-            return True
+        if state is not None:
+            return state == "true"
         else:
-            return False
+            return self.last_state
 
     def brightness(self):
         work_mode = self.data.get("color_mode")
@@ -15,6 +21,8 @@ class TuyaLight(TuyaDevice):
             brightness = int(self.data.get("color").get("brightness") * 255 / 100)
         else:
             brightness = self.data.get("brightness")
+        if brightness is None:
+            brightness = self.last_brightness
         return brightness
 
     def _set_brightness(self, brightness):
@@ -23,6 +31,7 @@ class TuyaLight(TuyaDevice):
             self.data["color"]["brightness"] = brightness
         else:
             self.data["brightness"] = brightness
+        self.last_brightness = brightness
 
     def support_color(self):
         return self.data.get("color") is not None or self.data.get("color_mode") == "colour"
@@ -35,7 +44,7 @@ class TuyaLight(TuyaDevice):
 
     def hs_color(self):
         if self.data.get("color") is None:
-            return None
+            return self.last_hs_color
         else:
             work_mode = self.data.get("color_mode")
             if work_mode == "colour":
@@ -46,7 +55,7 @@ class TuyaLight(TuyaDevice):
 
     def color_temp(self):
         if self.data.get("color_temp") is None:
-            return None
+            return self.last_color_temp
         else:
             return self.data.get("color_temp")
 
@@ -57,14 +66,17 @@ class TuyaLight(TuyaDevice):
         return 1000
 
     def turn_on(self):
+        self.last_state = True
         self.api.device_control(self.obj_id, "turnOnOff", {"value": "1"})
 
     def turn_off(self):
+        self.last_state = False
         self.api.device_control(self.obj_id, "turnOnOff", {"value": "0"})
 
     def set_brightness(self, brightness):
         """Set the brightness(0-255) of light."""
         value = int(brightness * 100 / 255)
+        self.last_brightness = value
         self.api.device_control(self.obj_id, "brightnessSet", {"value": value})
 
     def set_color(self, color):
@@ -79,9 +91,12 @@ class TuyaLight(TuyaDevice):
         # color white
         if hsv_color["saturation"] == 0:
             hsv_color["hue"] = 0
+        self.last_hs_color = (hsv_color["hue"], hsv_color["saturation"])
+        self.last_brightness = hsv_color["brightness"]
         self.api.device_control(self.obj_id, "colorSet", {"color": hsv_color})
 
     def set_color_temp(self, color_temp):
+        self.last_color_temp = color_temp
         self.api.device_control(
             self.obj_id, "colorTemperatureSet", {"value": color_temp}
         )
