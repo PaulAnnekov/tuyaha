ac8cc9bfa158856d8fdc9a0a8776dd45da5c6b82
Esteban Zapata Rojas <estebanz01@outlook.com>
2020-06-30 18:20:00 -0500

    tuyapi: Use Session object for every request made.
    This change implements the Session object, allowing us to reuse TCP
    connections via requests pool.

--- tuyaha/tuyaapi.py
+++ tuyaha/tuyaapi.py
@@ -14,6 +14,7 @@ DEFAULTREGION = "us"
 REFRESHTIME = 60 * 60 * 12
 
 _LOGGER = logging.getLogger(__name__)
+REQUEST_SESSION = requests.Session()
 
 
 class TuyaSession:
@@ -33,6 +34,7 @@ SESSION = TuyaSession()
 
 
 class TuyaApi:
+
     def init(self, username, password, countryCode, bizType=""):
         SESSION.username = username
         SESSION.password = password
@@ -48,7 +50,7 @@ class TuyaApi:
 
     def get_access_token(self):
         try:
-            response = requests.post(
+            response = REQUEST_SESSION.post(
                 (TUYACLOUDURL + "/homeassistant/auth.do").format(SESSION.region),
                 data={
                     "userName": SESSION.username,
@@ -94,7 +96,7 @@ class TuyaApi:
 
     def refresh_access_token(self):
         data = "grant_type=refresh_token&refresh_token=" + SESSION.refreshToken
-        response = requests.get(
+        response = REQUEST_SESSION.get(
             (TUYACLOUDURL + "/homeassistant/access.do").format(SESSION.region)
             + "?"
             + data
@@ -158,7 +160,7 @@ class TuyaApi:
         if namespace != "discovery":
             payload["devId"] = devId
         data = {"header": header, "payload": payload}
-        response = requests.post(
+        response = REQUEST_SESSION.post(
             (TUYACLOUDURL + "/homeassistant/skill").format(SESSION.region), json=data
         )
         if not response.ok:
